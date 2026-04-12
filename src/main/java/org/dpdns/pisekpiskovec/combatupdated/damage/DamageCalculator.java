cf0ca7bb7e7f351e91588de0e0cf4ba610f63d06
PisekPiskovec <pisekpiskovec@gmail.com>
2026-04-12 23:52:46 +0200

    feat(util): Add DamageCalculator for damage computation
    - Add DamageCalculator to compute damage based on risk levels and resistance modifiers.

---
+++ src/main/java/org/dpdns/pisekpiskovec/combatupdated/damage/DamageCalculator.java
@@ -0,0 +1,19 @@
+package org.dpdns.pisekpiskovec.combatupdated.damage;
+
+import org.dpdns.pisekpiskovec.combatupdated.api.ResistanceType;
+import org.dpdns.pisekpiskovec.combatupdated.api.RiskLevel;
+import org.dpdns.pisekpiskovec.combatupdated.util.RiskLevelDiffCalculator;
+
+public class DamageCalculator {
+    public static float calculate(float baseDamage, RiskLevel attackerRisk, RiskLevel defenderRisk, ResistanceType resistance, boolean isStaggered) {
+        // Damage modifier: Risk Level difference
+        float riskMod = RiskLevelDiffCalculator.getMultiplier(attackerRisk, defenderRisk);
+
+        // Damage modifier: Attack type resistance (or Stagger)
+        ResistanceType effectiveResistance = isStaggered ? ResistanceType.FATAL : resistance;
+        float resMod = (float) (effectiveResistance.getMultiplier() - 1.0);
+
+        // Final calculation
+        return Math.max(0f, baseDamage * (riskMod + resMod));
+    }
+}
