2907ffbb254a07df568723e2db6bf9692de56027
PisekPiskovec <pisekpiskovec@gmail.com>
2026-04-12 23:49:38 +0200

    refactor(api): rename parameter in setStaggered and add default method for effective resistance
    - Rename parameter in setStaggered method and add new default method for effective resistance calculation.

--- src/main/java/org/dpdns/pisekpiskovec/combatupdated/api/IAdvancedEntity.java
+++ src/main/java/org/dpdns/pisekpiskovec/combatupdated/api/IAdvancedEntity.java
@@ -2,8 +2,14 @@ package org.dpdns.pisekpiskovec.combatupdated.api;
 
 public interface IAdvancedEntity {
     RiskLevel getRiskLevel();
+
     ResistanceType getResistance(AttackType attackType);
+
     boolean isStaggered();
-    ResistanceType getStaggered();
-    void setStaggered(boolean isStaggered);
+
+    void setStaggered(boolean staggered);
+
+    default ResistanceType getEffectiveResistance(AttackType attackType) {
+        return isStaggered() ? ResistanceType.FATAL : getResistance(attackType);
+    }
 }
